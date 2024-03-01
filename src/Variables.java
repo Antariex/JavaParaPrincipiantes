bf9df5bc8d7830ca7b2092fc114d4aeb000e714c
Antariex <arielantequiera@gmail.com>
2024-03-01 19:24:39 -0300

    first commit

---
+++ src/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String args[]) {
+
+        int numero = 25;
+        String palabras = "Soy un string";
+        double temperatura = 38.5;
+        char letra = 'p';
+        boolean siONo = false;
+        long numeroLargo = 123456789;
+
+        int num1, num2, resultado;
+        num1 = 4;
+        num2 = 6;
+        resultado = num1 + num2;
+
+        System.out.println("El resultado es igual a: " + resultado);
+
+    }
+}
