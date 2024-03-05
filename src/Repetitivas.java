4257c1638a591ea7036943bb9c0044a3b57eefdf
Antariex <arielantequiera@gmail.com>
2024-03-05 07:44:16 -0300

    add for loop exercise

--- src/Repetitivas.java
+++ src/Repetitivas.java
@@ -1,10 +1,17 @@
 public class Repetitivas {
     public static void main(String[] args) {
-        int contador = 1;
 
-        while (contador <= 10) {
-            System.out.println("Estoy en la vuelta N° " + contador);
-            contador = contador + 1;
+        // While loop
+        int contador1 = 1;
+
+        while (contador1 <= 10) {
+            System.out.println("Contador1 está en la vuelta N° " + contador1);
+            contador1 = contador1 + 1; // De no incluirse esta línea, el contador no aumentaría y el bucle sería infinito al no producirse nunca la condición de salida.
+        }
+
+        // For loop
+        for (int contador2 = 1; contador2 <= 10; contador2++) {
+            System.out.println("Contador2 está en la vuelta N° " + contador2);
         }
     }
 }
