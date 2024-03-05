f78a88b570efe880520581da51af3e9c1bf8d58b
Antariex <arielantequiera@gmail.com>
2024-03-04 22:57:14 -0300

    add while exercise

---
+++ src/Repetitivas.java
@@ -0,0 +1,10 @@
+public class Repetitivas {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while (contador <= 10) {
+            System.out.println("Estoy en la vuelta N° " + contador);
+            contador = contador + 1;
+        }
+    }
+}
