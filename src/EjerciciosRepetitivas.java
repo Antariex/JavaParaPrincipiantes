a12dc09b98d51a2c467be97d23b1da36247ea562
Antariex <arielantequiera@gmail.com>
2024-03-05 08:41:09 -0300

    add loops practices

---
+++ src/EjerciciosRepetitivas.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class EjerciciosRepetitivas {
+    public static void main(String[] args) {
+        /*Ejercicio 1:
+
+        Realizar un programa que dado por teclado un límite númerico (por ejemplo 100)
+        muestre en pantalla todos los números hasta ese límite (empezando por 1).
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el número hasta el que quiere contar:");
+        int limite = scanner.nextInt();
+        int numero = 1;
+
+        while (numero <= limite) {
+            System.out.println(numero);
+            numero = numero + 1;
+        }*/
+
+        /*Ejercicio 2:
+
+        Realizar un programa que muestre en pantalla palabras que sean ingresadas por teclado hasta que se ingrese la palabra "salir".
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la palabra que quiera imprimir en pantalla. Para finalizar el programa escriba el comando 'salir':");
+
+        for (String palabra = scanner.next(); !palabra.equalsIgnoreCase("salir"); palabra = scanner.next()) {
+            System.out.println("El usuario ha ingresado la palabra: " + palabra);
+            System.out.println("Ingrese la palabra que quiera imprimir en pantalla. Para finalizar el programa escriba el comando 'salir':");
+        }*/
+    }
+}
