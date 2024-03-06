61a84f85768c06b3fc32743f24151d4a473f1276
Antariex <arielantequiera@gmail.com>
2024-03-06 09:42:18 -0300

    add loops practices

---
+++ src/EjerciciosRepetitivas4.java
@@ -0,0 +1,10 @@
+public class EjerciciosRepetitivas4 {
+    public static void main(String[] args) {
+        /*Ejercicio 4: arbolito de Navidad con ASCII.
+
+        Realizar un programa que de forma automática imprima por pantalla la imagen de un árbol de navidad
+        utilizando espacios, asteriscos y barras verticales.*/
+
+
+    }
+}
