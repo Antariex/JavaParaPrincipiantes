ed6abdd180eb448371ac282ad8c9344a70627c82
Antariex <arielantequiera@gmail.com>
2024-03-07 00:02:01 -0300

    add arrays examples

--- src/Arreglos.java
+++ src/Arreglos.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.Vector;
 
 public class Arreglos {
@@ -6,12 +7,26 @@ public class Arreglos {
         //Declaración
         int vector [] = new int [4];
 
-        //Asignación (manual)
+        /*Asignación (manual)
         vector [0] = 2;
         vector [1] = 35;
         vector [2] = 48;
-        vector [3] = 157;
+        vector [3] = 157;*/
+
+        //Asignación (por teclado)
+        Scanner teclado = new Scanner(System.in);
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Ingrese el valor para el índice " + i);
+            vector[i] = teclado.nextInt();
+        }
 
         //Los vectores siempre deben ser del mismo tipo de dato.
+
+        //Recorrido
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Estoy en el índice" + i);
+            System.out.println("Tengo guardado un " +  vector[i]);
+            System.out.println("-------");
+        }
     }
 }
