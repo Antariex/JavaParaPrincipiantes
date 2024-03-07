efd54d025422e823311ea148f312d1bbf9be405b
Antariex <arielantequiera@gmail.com>
2024-03-06 23:18:41 -0300

    add arrays examples

---
+++ src/Arreglos.java
@@ -0,0 +1,17 @@
+import java.util.Vector;
+
+public class Arreglos {
+    public static void main(String[] args) {
+
+        //Declaración
+        int vector [] = new int [4];
+
+        //Asignación (manual)
+        vector [0] = 2;
+        vector [1] = 35;
+        vector [2] = 48;
+        vector [3] = 157;
+
+        //Los vectores siempre deben ser del mismo tipo de dato.
+    }
+}
