a4c2858aff56f8c6f0c095b94c65ff4ee05735bb
Antariex <arielantequiera@gmail.com>
2024-03-07 08:35:19 -0300

    add arrays examples

--- src/Arreglos.java
+++ src/Arreglos.java
@@ -7,18 +7,18 @@ public class Arreglos {
         //Declaración vector
         int vector [] = new int [4];
 
-        /*Asignación (manual)
+        //Asignación (manual)
         vector [0] = 2;
         vector [1] = 35;
         vector [2] = 48;
-        vector [3] = 157;*/
+        vector [3] = 157;
 
         //Asignación (por teclado)
-        Scanner teclado = new Scanner(System.in);
+        /*Scanner teclado = new Scanner(System.in);
         for (int i = 0; i < vector.length; i++) {
             System.out.println("Ingrese el valor para el índice " + i);
             vector[i] = teclado.nextInt();
-        }
+        }*/
 
         //Los vectores siempre deben ser del mismo tipo de dato.
 
@@ -33,8 +33,31 @@ public class Arreglos {
         int matriz [][] = new int[3][3];
 
         //Asignación (manual)
-        matriz [0][0] = 5;
+        /*matriz [0][0] = 5;
         matriz [0][1] = 13;
-        matriz [2][2] = 24;
+        matriz [0][2] = 96;
+        matriz [1][0] = 35;
+        matriz [1][1] = 33;
+        matriz [1][2] = 71;
+        matriz [2][0] = 446;
+        matriz [2][1] = 228;
+        matriz [2][2] = 24;*/
+
+        //Asignación (por teclado)
+        Scanner teclado2 = new Scanner(System.in);
+        for (int f = 0; f < 3; f++) {
+            for (int c = 0; c < 3; c++) {
+                System.out.println("Ingrese el valor de la posición f:" + f + "c: " + c);
+                matriz[f][c] = teclado2.nextInt();
+            }
+        }
+
+        //Recorrido
+        for (int f = 0; f < 3; f++) {
+            for (int c = 0; c < 3; c++) {
+                System.out.println("El valor de la posición f:" + f + "c: " + c);
+                System.out.println("es de: " + matriz[f][c]);
+            }
+        }
     }
 }
