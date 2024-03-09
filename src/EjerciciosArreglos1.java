a337ff3fd0a3161824046f94dc9fd6cbacf5d0d6
Antariex <arielantequiera@gmail.com>
2024-03-08 22:39:10 -0300

    add arrays exercises

--- src/EjerciciosArreglos1.java
+++ src/EjerciciosArreglos1.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class EjerciciosArreglos1 {
     public static void main(String[] args) {
         /*
         Realizar un programa que permita cargar 15 números en un vector. Una vez cargados, se necesita que el
         programa cuente e informe por pantalla cuántas veces se cargóo el número 3.*/
+
+        int numeros [] = new int[15];
+        System.out.println("ingrese quince números para el vector");
+        Scanner ingresos = new Scanner(System.in);
+
+        for (int i = 0; i < 15; i++) {
+            numeros[i] = ingresos.nextInt();
+        }
+
+        int contador = 0;
+        for (int i = 0; i < 15; i++) {
+            if (numeros[i] == 3) {
+                contador = contador + 1;
+            }
+            }
+
+        System.out.println("Hay " + contador + " números tres en el vector.");
+        }
     }
-}
