fad95f03c4f8749407c64e0bb8cd5e08401d8ea9
Antariex <arielantequiera@gmail.com>
2024-03-08 22:13:12 -0300

    add arrays exercises

---
+++ src/EjerciciosArreglos1.java
@@ -0,0 +1,7 @@
+public class EjerciciosArreglos1 {
+    public static void main(String[] args) {
+        /*
+        Realizar un programa que permita cargar 15 números en un vector. Una vez cargados, se necesita que el
+        programa cuente e informe por pantalla cuántas veces se cargóo el número 3.*/
+    }
+}
