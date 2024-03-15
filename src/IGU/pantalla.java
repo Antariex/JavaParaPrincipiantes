25cdba1825bf87c254f9f09bd14f93330669170e
Antariex <arielantequiera@gmail.com>
2024-03-14 22:29:34 -0300

    add gui exercises

---
+++ src/IGU/pantalla.java
@@ -0,0 +1,9 @@
+package IGU;
+
+import javax.swing.*;
+
+public class pantalla {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton copíarButton;
+}
---
+++ src/PruebaCapas/PruebaCapas.java
@@ -0,0 +1,7 @@
+package PruebaCapas;
+
+public class PruebaCapas {
+    public static void main(String[] args) {
+        System.out.println("Hola!");
+    }
+}
